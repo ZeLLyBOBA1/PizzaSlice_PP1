82c6b2d3c7330096939815f2ed6444f78b49b642
Dmytro Loshch <loschdmitry2005@gmail.com>
2024-06-23 09:43:22 +0100

    project final stages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@ const navigationHeight = document.querySelector('header').offsetHeight;
 
 console.log(navigationHeight);
 
-document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 10 + "px");
 
 
 let popUpLabel = document.getElementById("pop-up-label");
